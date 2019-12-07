203b1cb4ae7066f226f3c040e486a3a213632eb1
Şeyma Özanar <ozanarseyma@gmail.com>
2019-12-07 14:25:27 +0300

    login

--- app/src/main/java/com/example/capstone_0443/Activities/LoginActivity.java
+++ app/src/main/java/com/example/capstone_0443/Activities/LoginActivity.java
@@ -1,34 +1,107 @@
 package com.example.capstone_0443.Activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 import com.example.capstone_0443.R;
-
-import java.security.PublicKey;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class LoginActivity extends AppCompatActivity {
-    EditText etPass, etUser;
-
-
+    EditText email, password;
+    private static final String TAG = " ";
+    TextView loginButton,registerButton;
+    private String userEmail, userPassword;
+    private FirebaseAuth auth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
-        etPass =  findViewById(R.id.etPass);
-        etUser =  findViewById(R.id.etUser);
+        password =  findViewById(R.id.password);
+        email =  findViewById(R.id.email);
+        loginButton = findViewById(R.id.loginButton);
+        registerButton = findViewById(R.id.registerButton);
+        auth = FirebaseAuth.getInstance();
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login();
+            }
+        });
+
+
+     }
+    private void login() {
+        userEmail = email.getText().toString().trim();
+        userPassword = password.getText().toString().trim();
+
+        auth.signInWithEmailAndPassword(userEmail, userPassword)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = auth.getCurrentUser();
+
+                            Intent intent = new Intent(LoginActivity.this,AndroidActivity.class);
+                            Toast.makeText(getApplicationContext(),"başarılı",Toast.LENGTH_SHORT).show();
+                            startActivity(intent);
+                            //updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(LoginActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                            // updateUI(null);
+                        }
+
+                        // ...
+                    }
+                });
+
+
+    }
+
     }
 
 
+/*
 
     public void onClick(View view) { //Butonlara tıklanınca
 
@@ -82,6 +155,6 @@ public class LoginActivity extends AppCompatActivity {
             });
             builder.show();
         }
-    }
-}
+    }*/
+
 
